674bf0e1eadef60ce721e52261d93f73514d82a8
arapl3y <rapley3@gmail.com>
2018-07-03 21:43:18 +1000

    Add handle message and send message methods

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 require('dotenv').config()
-
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const handleMessage = require('./handlers').handleMessage;
+const handlePostback = require('./handlers').handlePostback;
 
 app = express().use(bodyParser.json());
 
@@ -32,6 +33,13 @@ app.post('/webhook', (req, res) => {
       console.log(webhookEvent);
 
       let senderPsid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPsid );
+
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
+      }
     });
     res.status(200).send('EVENT_RECEIVED');
   } else {
