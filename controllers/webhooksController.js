ffc5f2cd65d7138a78c8185d9a488dd24de53e4a
arapl3y <rapley3@gmail.com>
2018-07-10 22:21:52 +1000

    Move webhook controller methods to separate file

---
+++ controllers/webhooksController.js
@@ -0,0 +1,42 @@
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const { handleMessage } = require('../handlers/handlers.js');
+
+function getWebhook(req, res) {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+}
+
+function postWebhook(req, res) {
+  let body = req.body;
+
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhookEvent = entry.messaging[0];
+
+      let senderPsid = webhookEvent.sender.id;
+
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        console.log('postback received');
+        // handlePostback(senderPsid, webhookEvent.postback);
+      }
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+}
+
+module.exports.postWebhook = postWebhook;
+module.exports.getWebhook = getWebhook;
